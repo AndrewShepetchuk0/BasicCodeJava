135a3567e296834cd1eb600c49225291ab04ee52
AndrewShepetchuk0 <109487801+AndrewShepetchuk0@users.noreply.github.com>
2024-11-17 00:09:47 -0500

    Add files via upload

---
+++ a13.java
@@ -0,0 +1,16 @@
+package assignment1;
+import java.util.*;
+
+public class a13 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the radius of a sphere: ");
+		double r = input.nextDouble();
+		double a = 4*(Math.PI*Math.pow(r, 2));
+		double v = 4/3*(Math.PI*Math.pow(r, 3));
+		System.out.println("The area of the sphere is: " + a);
+		System.out.println("The volume of the spphere is: " + v);
+	}
+
+}
