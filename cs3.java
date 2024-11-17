eee65b1fedffd85248d7cbba86f173580abf64dd
AndrewShepetchuk0 <109487801+AndrewShepetchuk0@users.noreply.github.com>
2024-11-16 20:28:35 -0500

    Add files via upload

---
+++ cs3.java
@@ -0,0 +1,14 @@
+package cs1;
+import java.util.*;
+
+public class cs3 {
+	public static void main(String[] args) {
+		Random ran = new Random();
+		Scanner input = new Scanner(System.in);
+		int n = ran.nextInt(100);
+		System.out.println("Guess the number");
+		int num = input.nextInt();	
+		while(num < n || num > n) {
+			if(num < n) {System.out.println("Too low. Try again");num = input.nextInt();}
+			if(num > n) {System.out.println("Too high. Try again");num = input.nextInt();}}
+		System.out.println("You guessed right");input.close();}}
